b68ad6e5dbfb5de7debabd387998d41d75bf54c8
dwnste <dwnste@gmail.com>
2017-06-05 12:17:25 +0300

    added morephotos button

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ import moment from 'moment'
 
 moment.locale('ru');
 
+let myPlacemark,
+        myMap,
+        photoWrapper,
+        previousQueryArgs,
+        morePhotosButton,
+        count;
+
+
+const cleanPhotoWrapper = () => {
+    photoWrapper.innerHTML = '';
+}
+
+const morePhotosButtonClick = () => {
+    if (previousQueryArgs && count > photoWrapper.childElementCount) {
+        getPhotos(previousQueryArgs.lat, previousQueryArgs.long, undefined, undefined, photoWrapper.childElementCount);
+    }
+}
 
 const getPhotos = (lat, long, radius = 1000, count = 50, offset = 0) => {
-    const url = `//api.vk.com/method/photos.search?lat=${lat}&long=${long}&radius=${radius}&count=${count}`;
+    const url = `//api.vk.com/method/photos.search?lat=${lat}&long=${long}&radius=${radius}&count=${count}&offset=${offset}`;
     fetchJsonp(url)
     .then(function(response) {
         return response.json()
@@ -30,30 +47,53 @@ const createPlacemark = (coords) => {
 
 
 const renderContent = (result) => {
-    const [count, ...photos] = result.response;
-    const photoWrapper = document.getElementById('photoWrap');
-    photoWrapper.innerHTML = '';
-
+    let photos;
+    [count, ...photos] = result.response;
+    console.log(`count from render content ${count}`)
     for (let element of photos) {
         const date = moment(element.created*1000).format('L')
         photoWrapper.innerHTML+=`<div class="image"><img src="${element.src}"><a href="${element.src_big}" target="_blank"><h2><span>${date}</span></h2></a></div>`
     }
 }
 
+// Определяем адрес по координатам (обратное геокодирование).
+const getAddress = (coords) => {
+    myPlacemark.properties.set('iconCaption', 'поиск...');
+    ymaps.geocode(coords).then(function (res) {
+        var firstGeoObject = res.geoObjects.get(0);
+        myPlacemark.properties
+            .set({
+                // Формируем строку с данными об объекте.
+                iconCaption: [
+                    // Название населенного пункта или вышестоящее административно-территориальное образование.
+                    firstGeoObject.getLocalities().length ? firstGeoObject.getLocalities() : firstGeoObject.getAdministrativeAreas(),
+                    // Получаем путь до топонима, если метод вернул null, запрашиваем наименование здания.
+                    firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
+                ].filter(Boolean).join(', '),
+                // В качестве контента балуна задаем строку с адресом объекта.
+                balloonContent: firstGeoObject.getAddressLine()
+            });
+    });
+}
+
 
 const init = () => {
-    let myPlacemark,
-        myMap = new ymaps.Map('map', {
-            center: [55.753994, 37.622093],
-            zoom: 9
-        }, {
-            searchControlProvider: 'yandex#search'
-        });
+    photoWrapper = document.getElementById('photoWrap');
+    morePhotosButton = document.getElementById('morePhotosButton');
+    myMap = new ymaps.Map('map', {
+        center: [55.753994, 37.622093],
+        zoom: 9
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
     // Слушаем клик на карте.
+    morePhotosButton.addEventListener('click', morePhotosButtonClick);
     myMap.events.add('click', (e) => {
         const coords = e.get('coords');
         const [lat, long] = coords;
+        cleanPhotoWrapper();
         getPhotos(lat, long);
+        previousQueryArgs = {lat, long};
         // Если метка уже создана – просто передвигаем ее.
         if (myPlacemark) {
             myPlacemark.geometry.setCoordinates(coords);
@@ -69,27 +109,6 @@ const init = () => {
         }
         getAddress(coords);
     });
-
-
-    // Определяем адрес по координатам (обратное геокодирование).
-    const getAddress = (coords) => {
-        myPlacemark.properties.set('iconCaption', 'поиск...');
-        ymaps.geocode(coords).then(function (res) {
-            var firstGeoObject = res.geoObjects.get(0);
-            myPlacemark.properties
-                .set({
-                    // Формируем строку с данными об объекте.
-                    iconCaption: [
-                        // Название населенного пункта или вышестоящее административно-территориальное образование.
-                        firstGeoObject.getLocalities().length ? firstGeoObject.getLocalities() : firstGeoObject.getAdministrativeAreas(),
-                        // Получаем путь до топонима, если метод вернул null, запрашиваем наименование здания.
-                        firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
-                    ].filter(Boolean).join(', '),
-                    // В качестве контента балуна задаем строку с адресом объекта.
-                    balloonContent: firstGeoObject.getAddressLine()
-                });
-        });
-    }
 }
 
 ymaps.ready(init);
