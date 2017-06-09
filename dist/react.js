fd3eb47c7add6b57bb178c77cd9a98bf5d645377
dwnste <dwnste@gmail.com>
2017-06-09 17:43:30 +0300

    немного поправил верстку

--- dist/react.js
+++ dist/react.js
@@ -90,6 +90,7 @@ class App extends Component {
     }
 
     render() {
+        console.log(this.state.photos)
         return <InfiniteScroll
             pageStart={0}
             loadMore={this.loadItems.bind(this)}
