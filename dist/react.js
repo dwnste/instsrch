d3ec9ab91f3255f998488165c4ed625c09adbccc
dwnste <dwnste@gmail.com>
2017-06-09 15:13:04 +0300

    вынес инициализацию карты в componentDidMount

--- dist/react.js
+++ dist/react.js
@@ -9,9 +9,40 @@ moment.locale('ru');
 
 const MAP_CENTER = [55.753994, 37.622093];
 
+let myPlacemark,
+    myMap;
+
+const updateMyPlacemark = (coords) => {
+    myPlacemark.geometry
+        .setCoordinates(coords);
+    myPlacemark.properties
+        .set('iconCaption', 'поиск...');
+    getGeoObject(coords)
+        .then(firstGeoObject =>
+            myPlacemark.properties
+                .set({
+                    // Формируем строку с данными об объекте.
+                    iconCaption: [
+                        /* Название населенного пункта или
+                        вышестоящее административно-территориальное образование. */
+                        firstGeoObject.getLocalities().length
+                            ? firstGeoObject.getLocalities()
+                            : firstGeoObject.getAdministrativeAreas(),
+                        /* Получаем путь до топонима, если метод вернул null,
+                        запрашиваем наименование здания. */
+                        firstGeoObject.getThoroughfare()
+                        || firstGeoObject.getPremise(),
+                    ].filter(Boolean)
+                    .join(', '),
+                    // В качестве контента балуна задаем строку с адресом объекта.
+                    balloonContent: firstGeoObject.getAddressLine(),
+                }),
+    );
+};
+
 class App extends Component {
-    constructor(props) {
-        super(props);
+    constructor(...props) {
+        super(...props);
 
         this.state = {
             available: 0,
@@ -21,74 +52,6 @@ class App extends Component {
             offset: 0,
             count: 50,
         };
-
-        let myPlacemark,
-            myMap;
-
-        const updateMyPlacemark = (coords) => {
-            myPlacemark.geometry
-                .setCoordinates(coords);
-            myPlacemark.properties
-                .set('iconCaption', 'поиск...');
-            getGeoObject(coords)
-                .then(firstGeoObject =>
-                    myPlacemark.properties
-                        .set({
-                            // Формируем строку с данными об объекте.
-                            iconCaption: [
-                                /* Название населенного пункта или
-                                вышестоящее административно-территориальное образование. */
-                                firstGeoObject.getLocalities().length
-                                    ? firstGeoObject.getLocalities()
-                                    : firstGeoObject.getAdministrativeAreas(),
-                                /* Получаем путь до топонима, если метод вернул null,
-                                запрашиваем наименование здания. */
-                                firstGeoObject.getThoroughfare()
-                                || firstGeoObject.getPremise(),
-                            ].filter(Boolean)
-                            .join(', '),
-                            // В качестве контента балуна задаем строку с адресом объекта.
-                            balloonContent: firstGeoObject.getAddressLine(),
-                        }),
-            );
-        };
-
-        const init = () => {
-            // инициализация карты
-            myMap = new ymaps.Map('map', {
-                center: MAP_CENTER,
-                zoom: 9,
-            }, {
-                searchControlProvider: 'yandex#search',
-            });
-
-
-            // ставим метку и грузим фотографии, когда карта загрузилась
-            const coords = MAP_CENTER;
-            myPlacemark = createPlacemark(coords);
-            myMap.geoObjects.add(myPlacemark);
-            updateMyPlacemark(coords);
-
-
-            myMap.events.add('click', (e) => {
-                const coords = e.get('coords');
-                updateMyPlacemark(coords);
-                this.setState({
-                    coords, offset: 0, photos: [],
-                });
-            });
-
-
-            myPlacemark.events.add('dragend', () => {
-                const coords = myPlacemark.geometry.getCoordinates();
-                updateMyPlacemark(coords);
-                this.setState({
-                    coords, offset: 0, photos: [],
-                });
-            });
-        };
-
-        ymaps.ready(init);
     }
 
     loadItems(page) {
@@ -134,12 +97,47 @@ class App extends Component {
                 loadMore={this.loadItems.bind(this)}
                 hasMore={this.state.hasMoreItems}
                 loader={loader}
-                useWindow={false}>
-
+                useWindow={true}>
                 
                 {items}
             </InfiniteScroll>
         );
+    }
+    componentDidMount() {
+        ymaps.ready(() => {
+            // инициализация карты
+            myMap = new ymaps.Map('map', {
+                center: MAP_CENTER,
+                zoom: 9,
+            }, {
+                searchControlProvider: 'yandex#search',
+            });
+
+
+            // ставим метку и грузим фотографии, когда карта загрузилась
+            const coords = MAP_CENTER;
+            myPlacemark = createPlacemark(coords);
+            myMap.geoObjects.add(myPlacemark);
+            updateMyPlacemark(coords);
+
+
+            myMap.events.add('click', (e) => {
+                const coords = e.get('coords');
+                updateMyPlacemark(coords);
+                this.setState({
+                    coords, offset: 0, photos: [],
+                });
+            });
+
+
+            myPlacemark.events.add('dragend', () => {
+                const coords = myPlacemark.geometry.getCoordinates();
+                updateMyPlacemark(coords);
+                this.setState({
+                    coords, offset: 0, photos: [],
+                });
+            });
+        });
     }
 }
 
